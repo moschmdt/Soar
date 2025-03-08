09de8ca3bd54a79a74a7223ad4192206998d1e60
Nathan Glenn <garfieldnate@gmail.com>
2025-03-07 22:53:08 -0600

    current_path() not supported on MacOS 13
    Just don't use filesystem for now. Windows and Mac both have problems
    with it.

--- Core/CLI/src/cli_CommandLineInterface.cpp
+++ Core/CLI/src/cli_CommandLineInterface.cpp
@@ -6,7 +6,6 @@
 #include <cassert>
 
 #include <iostream>
-#include <filesystem>
 #include <fstream>
 #include <cctype>
 
@@ -90,7 +89,7 @@ EXPORT CommandLineInterface::CommandLineInterface()
     m_Parser.AddCommand(new cli::WMCommand(*this));
     m_Parser.AddCommand(new cli::SVSCommand(*this));
 
-    m_HomeDirectory = std::filesystem::current_path().string();
+    GetCurrentWorkingDirectory(m_HomeDirectory);
 
     Soar_Instance::Get_Soar_Instance().Set_CLI(this);
 }
@@ -363,6 +362,7 @@ EXPORT void CommandLineInterface::SetKernel(sml::KernelSML* pKernelSML)
     m_pKernelSML = pKernelSML;
 }
 
+// TODO: migrate to std::filesystem::path once supported everywhere
 bool CommandLineInterface::GetCurrentWorkingDirectory(std::string& directory)
 {
     // Pull an arbitrary buffer size of 1024 out of a hat and use it
